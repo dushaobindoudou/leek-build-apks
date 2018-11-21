e964c4ac7ac17963ac2c233fae17404a43357339
dushaobin <dushaobin@we.com>
2018-11-21 19:53:39 +0800

    添加websocket支持

---
+++ autoRun.js
@@ -0,0 +1,147 @@
+
+const ReconnectWebSocket = require('reconnect-websocket');
+const utils = require('./utils');
+
+/**
+ * 获取可用的ws 链接实例，如果链接不可用，
+ */
+function getStatbleWSC() {
+    const conf = utils.getConfigInfo();
+    if (!conf || !conf.websocketServer) {
+        return null;
+    }
+    return new ReconnectWebSocket(conf.websocketServer, null, {
+        reconnectAttempt: true,
+        maxReconnectAttempts: 30000,
+    });
+}
+
+function createWSC(onOpen, onMsg, onError, onClose) {
+    const ws = getStatbleWSC();
+
+    if (!ws) {
+        console.log('创建 websocket 客户端失败，可能是没有配置websocket 服务地址');
+        return null;
+    }
+
+    ws.on('open', () => {
+        ws.send('client is connected success');
+        onOpen && onOpen(ws);
+    });
+
+    ws.on('message', (msg) => {
+        console.log('message from server:', msg);
+        onMsg && onMsg(ws, msg);
+    });
+
+    ws.on('error', (msg) => {
+        console.log('ws error:', msg);
+        onError && onError(ws, msg);
+    });
+
+    ws.on('close', (msg) => {
+        console.log('ws close:', msg);
+        onClose && onClose(ws, msg);
+    });
+
+    return ws;
+}
+
+function getTaskList() {
+    return utils.getBuildLog();
+}
+
+function sendMsg(ws, msg) {
+    ws.send(JSON.stringify(msg));
+}
+
+module.exports = {
+    init: (ready) => {
+        const wsc = createWSC((ws) => {
+            console.log('ws client start success');
+            utils.addListener('buildSucc', (msg) => {
+                console.log('成功构建：', msg);
+                ws.send(JSON.stringify({
+                    type: 'buildSucc',
+                    curr: msg,
+                    full: getTaskList(),
+                }));
+            });
+            console.log('websocket client start successed！');
+            if (ready) {
+                ready();
+            }
+        }, (msg) => {
+            try {
+                if (!msg) {
+                    return;
+                }
+                const msgO = JSON.parse(msg);
+                console.log('ws server msg:', msgO);
+                switch (msgO.type) {
+                case 'install':
+                    const res = utils.installApk(msgO.deviceId, msgO.apkPath);
+                    sendMsg({
+                        type: 'install',
+                        data: res,
+                    });
+                    break;
+                case 'uninstall':
+                    const res = utils.uninstallApk(msgO.deviceId, msg0.packageName);
+                    sendMsg({
+                        type: 'uninstall',
+                        data: res,
+                    });
+                    break;
+                case 'openActivity':
+                    const res = utils.openActivity(msgO.deviceId, msg0.activity);
+                    sendMsg({
+                        type: 'openActivity',
+                        data: res,
+                    });
+                    break;
+                case 'stopApp':
+                    const res = utils.stopApp(msgO.deviceId, msg0.packageName);
+                    sendMsg({
+                        type: 'stopApp',
+                        data: res,
+                    });
+                    break;
+                case 'grandPermission':
+                    const res = utils.grantPermission(msgO.deviceId, msg0.packageName, msg0.permission);
+                    sendMsg({
+                        type: 'grandPermission',
+                        data: res,
+                    });
+                    break;
+                case 'deviceList':
+                    sendMsg({
+                        type: 'deviceList',
+                        data: utils.getDevicesList(),
+                    });
+                    break;
+                case 'emulatorList': 
+                    sendMsg({
+                        type: 'emulatorList',
+                        data: utils.getEmulatorList(),
+                    });
+                    break;
+                case 'clientEnv' :
+                    sendMsg({
+                        type: 'clientEnv',
+                        data: utils.checkSdkEnv(),
+                    });
+                    break;
+                default:
+                    console.log('wss 消息没有对应的指令：', msgO);
+                }
+            } catch (err) {
+                console.log('parser server msg:', err);
+            }
+        }, (err) => {
+            console.log('ws client error', err);
+        }, () => {
+            console.log('ws client is closed!');
+        });
+    },
+};
